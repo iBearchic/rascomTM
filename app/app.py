f39ee1616bf58fa6c2895d6c128d5119b7d104c3
avenue-spb <maikopm1901@yandex.ru>
2023-05-17 02:20:23 +0300

    perform changes in app.py

--- app/app.py
+++ app/app.py
@@ -1,36 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-import os
-from manage import User
+from config import Config
 
-# import the blueprints
-from views import main_blueprint
-from auth import auth_blueprint
+db = SQLAlchemy()
 
-app = Flask(__name__)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-# configuration settings
-app.config['SECRET_KEY'] = 'asdfghjkl qwertyui'  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
+    from views import main_blueprint
+    app.register_blueprint(main_blueprint)
 
-# register the blueprints
-app.register_blueprint(main_blueprint)
-app.register_blueprint(auth_blueprint)
+    from auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    return app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = create_app()
+    app.run()
+
 
