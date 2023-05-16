bc773d88d1aec97b79482634bf7835764c2afadf
avenue-spb <maikopm1901@yandex.ru>
2023-05-17 02:00:23 +0300

    forms.py created

---
+++ app/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from manage import User, Employee, Task
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+class TaskForm(FlaskForm):
+    taskname = StringField('Task Name', validators=[DataRequired()])
+    expected_time = IntegerField('Expected Time', validators=[DataRequired()])
+    task_hardness = IntegerField('Task Hardness', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class AssignTaskForm(FlaskForm):
+    time_assigned = IntegerField('Time Assigned', validators=[DataRequired()])
+    submit = SubmitField('Assign')
