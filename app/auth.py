557d938cdac3109aec003d35e5dd06e92f81e8fc
avenue-spb <maikopm1901@yandex.ru>
2023-05-17 01:56:15 +0300

    auth.py created

---
+++ app/auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from manage import User, db
+from forms import LoginForm, RegisterForm
+
+auth_blueprint = Blueprint('auth', __name__)
+
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('main.home'))
+        flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
+@auth_blueprint.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=form)
+
+@auth_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
